5b189d92e27d223f7d61dd431557b1d3df086d82
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-06-03 08:54:22 -0700

    some updates

--- tets/src/com/pluralsight/calcengine/Main.java
+++ tets/src/com/pluralsight/calcengine/Main.java
@@ -2,30 +2,79 @@ package com.pluralsight.calcengine;
 
 import java.io.*;
 import java.util.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.HashMap;
 
 public class Main {
 
     public static void main(String[] args) {
-        int[] inputArr = new int[]{2,4,6,8};
-        int value = 5;
-        double midpoint = (double) inputArr.length/ 2;
-        int roundedMidpoint = (int)(Math.round(midpoint));
-        System.out.println(roundedMidpoint);
-        System.out.println(Arrays.toString(insertShiftArray(inputArr, value)));
+        int[][] weeklyMonthTemperatures = {
+                {66, 64, 58, 65, 71, 57, 60},
+                {57, 65, 65, 70, 72, 65, 51},
+                {55, 54, 60, 53, 59, 57, 61},
+                {65, 56, 55, 52, 55, 62, 57}
+        };
+
+//        weatherData(weeklyMonthTemperatures);
+
+        ArrayList<String> votes = new ArrayList<String>();
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Bush");
+        votes.add("Shrub");
+        votes.add("Hedge");
+        votes.add("Shrub");
+        votes.add("Bush");
+        votes.add("Hedge");
+        votes.add("Bush");
+
+      tally(votes);
     }
 
-    public static int[] insertShiftArray(int[] inputArr, int value) {
-        float midpoint = (float) inputArr.length / 2;
-        int roundedMidpoint = Math.round(midpoint);
-        int[] newArray = new int[inputArr.length + 1];
 
-        for (int i = 0; i < roundedMidpoint; i++) {
-            newArray[i] = inputArr[i];
+    public static void weatherData(int[][] inputArr) {
+        HashSet<Integer> array = new HashSet<>();
+        HashSet<Integer> result = new HashSet<>();
+        for(int i = 0; i < inputArr.length; i++) {
+            for(int j = 0; j < inputArr[i].length; j++) {
+                array.add(inputArr[i][j]);
+            }
         }
-        newArray[roundedMidpoint] = value;
-        for (int j = inputArr.length - 1; j >= roundedMidpoint; j--) {
-            newArray[j + 1] = inputArr[j];
+        int max = Collections.max(array);
+        int min = Collections.min(array);
+        result.add(max);
+        result.add(min);
+
+        for(int i = min+1; i < max - 1; i++){
+            if(!array.contains(i)){
+                result.add(i);
+            }
         }
-        return newArray;
+
+        // Creating an Array
+        String[] resultArray = new String[result.size()];
+        result.toArray(resultArray);
+
+        System.out.println(resultArray);
+    }
+
+    public static void tally(ArrayList<String> list) {
+        HashMap<String, Integer> characterCounts = new HashMap<>();
+        String[] characters = new String[]{"Bush", "Shrub", "Hedge"};
+        for (String c : characters) {
+            characterCounts.put(c, 0);
+        }
+        for (String character : list) {
+            if (list.contains(character)) {
+                int countSoFar = characterCounts.get(character);
+                characterCounts.put(character, countSoFar + 1);
+            }
+        }
+
+        String result =Collections.max(characterCounts.entrySet(), Map.Entry.comparingByValue()).getKey();
+        System.out.println(result);
     }
 }
