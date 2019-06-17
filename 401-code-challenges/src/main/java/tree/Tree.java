0a7c80d465e0592ae7e18c5fb04158259bde379a
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-06-17 16:51:11 -0700

    test completed

--- 401-code-challenges/src/main/java/tree/Tree.java
+++ 401-code-challenges/src/main/java/tree/Tree.java
@@ -11,69 +11,39 @@ public class Tree {
         this.root = null;
     }
 
+    public Tree(Node root) {
+        this.root = root;
+    }
+
 //    https://www.geeksforgeeks.org/tree-traversals-inorder-preorder-and-postorder/
 
-    ArrayList<Integer> preOrder = new ArrayList<Integer>();
-    public void preOrder(Node node) {
-        if(node == null){
-            return;
-        }
-        System.out.print(node.value + " ");
-        preOrder(node.left);
-        preOrder(node.right);
+    public ArrayList preOrder(Node node, ArrayList<Integer> preOrderArray) {
+        if (node == null) {
+                return null;
+            }
+            preOrderArray.add(node.value);
+            preOrder(node.left, preOrderArray);
+            preOrder(node.right, preOrderArray);
+            return preOrderArray;
     }
 
-    public void inOrder(Node node) {
+    public ArrayList inOrder(Node node, ArrayList<Integer> inOrderArray) {
         if(node == null) {
-            return;
+            return null;
         }
-        inOrder(node.left);
-        System.out.println(node.value+ " ");
-        inOrder(node.right);
+        inOrder(node.left, inOrderArray);
+        inOrderArray.add(node.value);
+        inOrder(node.right, inOrderArray);
+        return inOrderArray;
     }
 
-    public void postOrder(Node node) {
+    public ArrayList postOrder(Node node, ArrayList<Integer> postOrderArray) {
         if(node == null){
-            return;
+            return null;
         }
-        postOrder(node.left);
-        postOrder(node.right);
-        System.out.println(node.value + " ");
+        postOrder(node.left, postOrderArray);
+        postOrder(node.right, postOrderArray);
+        postOrderArray.add(node.value);
+        return postOrderArray;
     }
-
-    public static void main(String[] args)
-    {
-        Tree tree = new Tree();
-        tree.root = new Node(8);
-        tree.root.left = new Node(3);
-        tree.root.right = new Node(10);
-        tree.root.left.left = new Node(1);
-        tree.root.left.right = new Node(6);
-        tree.root.right.right = new Node(14);
-
-        System.out.println("Preorder traversal of binary tree is ");
-        tree.preOrder(tree.root);
-
-        System.out.println("\nInorder traversal of binary tree is ");
-        tree.inOrder(tree.root);
-
-        System.out.println("\nPostorder traversal of binary tree is ");
-        tree.postOrder(tree.root);
-
-
-        BinarySearchTree newTree = new BinarySearchTree();
-        newTree.add( 3);
-        newTree.add( 1);
-        newTree.add( 6);
-        newTree.add(10);
-        newTree.add( 9);
-        newTree.add(14);
-
-        System.out.println("Inorder traversal of binary tree is after adding ");
-        tree.inOrder(newTree.root);
-
-        System.out.println(newTree.contains(9));
-
-    }
-
 }
