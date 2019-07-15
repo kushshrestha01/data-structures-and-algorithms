e5740ee2832a95ff7fb7d0533a3fa22cec5db5b0
Kush Shrestha <kush_shrestha01@yahoo.com>
2019-07-15 11:32:15 -0700

    lab completed needs test and readme

---
+++ 401-code-challenges/src/main/java/graph/Edge.java
@@ -0,0 +1,12 @@
+package graph;
+
+public class Edge {
+
+    int weight;
+    Node node;
+
+    public Edge(int weight, Node node) {
+        this.weight = weight;
+        this.node = node;
+    }
+}
